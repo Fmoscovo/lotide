4f15d00056dc1bb82175eab53ba9af9b8c979b99
Filipe Moscovo <your@email.com>
2023-06-29 09:53:27 -0600

    Create letterPosition.js

---
+++ letterPosition.js
@@ -0,0 +1,50 @@
+
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: [${array1}] is equal to [${array2}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${array1}] is not equal to [${array2}]`);
+  }
+};
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+       if (array1[i] !== array2[i]) {
+         return false;
+       }
+    
+  }
+  return true;
+      };
+
+
+
+const letterPositions = function(sentence) {
+  const results = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    const position = sentence[i];
+
+    if (position !== ' ') {
+      if (results[position]) {
+        results[position].push(i);
+      } else {
+        results[position] = [i];
+      }
+    }
+  }
+
+  return results;
+};
+
+const testSentence = 'hello';
+const result = letterPositions(testSentence);
+
+assertArraysEqual(result['h'], [0]);
+assertArraysEqual(result['e'], [1]);
+assertArraysEqual(result['l'], [2, 3]);
+assertArraysEqual(result['o'], [4]);
